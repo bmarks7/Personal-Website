e32581248a90c638037a34f8b34eac8216b2a29e
Brandon Nathan Marks <bmarks7@uwo.ca>
2022-02-04 10:55:04 -0500

    updated resume

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -18,7 +18,7 @@ export default class Resume extends Component {
                 
                 <div className="Resume__Container">
                     <Header text='My Resume'/>
-                    <iframe className='Resume__Container__iframe' title='resume' src="https://drive.google.com/file/d/1sBsKwA2b6fbKXsC-8A8v7_dZRsLCEEQd/preview" allow="autoplay"></iframe>
+                    <iframe className='Resume__Container__iframe' title='resume' src="https://drive.google.com/file/d/1xxNblnJRFJBu8s1ll_7jZBV9uHIU26Qo/preview" allow="autoplay"></iframe>
                 </div>
 
             </div>
