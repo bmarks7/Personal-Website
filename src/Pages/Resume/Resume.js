cc09435af7e057a95131aa53c82e39a52abbcd4d
Brandon Nathan Marks <bmarks7@uwo.ca>
2023-03-15 02:37:44 -0400

    updated resume src

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react'
-import Header from '../../Components/Header/Header';
-import SocialLink from '../../Components/SocialLink/SocialLink';
-import googleDriveIcon from '../../Images/googleDrive.png';
+import React, { Component } from "react";
+import Header from "../../Components/Header/Header";
+import SocialLink from "../../Components/SocialLink/SocialLink";
+import googleDriveIcon from "../../Images/googleDrive.png";
 
-import './Resume.scss'
+import "./Resume.scss";
 
 export default class Resume extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPage: null,
+      pageNUmber: 1,
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            numPage: null,
-            pageNUmber: 1,
-        }
-    }
-
-    render() {
-        return (
-            <div className='Resume'>
-                
-                <div className="Resume__Container">
-                    <Header text='Here is my updated resume'/>
-                    <iframe data-aos='fade-right' className='Resume__Container__iframe' title='resume' src="https://drive.google.com/file/d/1xxNblnJRFJBu8s1ll_7jZBV9uHIU26Qo/preview" allow="autoplay"></iframe>
-                    <div className="Resume__Container__driveLink">
-                        <SocialLink url = 'https://drive.google.com/file/d/1xxNblnJRFJBu8s1ll_7jZBV9uHIU26Qo/view?usp=sharing' name='Link' icon={googleDriveIcon}/>
-                    </div>
-                </div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Resume">
+        <div className="Resume__Container">
+          <Header text="Here is my updated resume" />
+          <iframe
+            data-aos="fade-right"
+            className="Resume__Container__iframe"
+            title="resume"
+            src="https://drive.google.com/file/d/1A8uR4nnXR-cEZoRK0gfyOvHwGrseZ7mT/preview"
+            allow="autoplay"
+          ></iframe>
+          <div className="Resume__Container__driveLink">
+            <SocialLink
+              url="https://drive.google.com/file/d/1xxNblnJRFJBu8s1ll_7jZBV9uHIU26Qo/view?usp=sharing"
+              name="Link"
+              icon={googleDriveIcon}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
