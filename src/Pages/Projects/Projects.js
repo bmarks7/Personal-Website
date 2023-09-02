0bb6f7102bfb585b0ac8315045750ff9f33664ab
Brandon Nathan Marks <bmarks7@uwo.ca>
2023-09-01 22:14:51 -0400

    edited project descriptions

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -15,7 +15,7 @@ export default class Other extends Component {
           name="SpeechFinder"
           event="HawkHacks"
           duration="May 2022"
-          description="SpeechFinder brings underheard voices to the center of a stronger public dialogue by allowing any user to contribute stories for civic action. They do so by uploading an audio file, which is run through AssemblyAI's API to extract keywords, phrases and entities, and transcribe the audio file. Then journalists can search the stories in the database using keywords and even filter for local stories to recognize patterns over time and in-the-moment trends or discover unmet community needs worth investigating. The results provide keywords and audio transcript excerpts to give an idea of what the story is about. When clicking a result, journalists and other users can find a full audio transcript and use all of the information they find for many social purposes."
+          description="A web application that brings underheard voices to the center of a stronger public dialogue by allowing users — whether individuals or nonprofits — to contribute stories for civic action"
           skills={["React", "Python", "Flask", "SQLite"]}
           links={[
             {
@@ -36,7 +36,7 @@ export default class Other extends Component {
           name="EduFlash"
           event="RU Hacks"
           duration="May 2022"
-          description="An education platform that increases student engagement and learning outcomes through gamification. Students are awarded points for earning good marks on quizzes and participating regularly, and these points result in additional marks for students. Using Twilio API, our application sends reminders and notifications to students through SMS messages."
+          description="A web application serving as an education platform that increases student engagement and learning outcomes through gamification."
           skills={["React", "Python", "Flask"]}
           links={[
             {
@@ -57,7 +57,7 @@ export default class Other extends Component {
           name="UmmLess.tech"
           event="DeltaHacks"
           duration="Jan 2022"
-          description="A web application that helps people improve their public speaking skills by analyzing audio and using AssemblyAI API"
+          description="A web application that helps people improve their public speaking skills by analyzing audio using AssemblyAI API"
           skills={["React", "Flask", "SQLite"]}
           links={[
             {
@@ -78,7 +78,7 @@ export default class Other extends Component {
           name="Ghost Runner"
           event="Hack Western"
           duration="Nov 2020"
-          description="Our application allows users to record their runs in the form of ghosts (playable recorded runs) which record the distance that a user travels at certain times and allows their runs to be replayed and challenged by other users to beat their times. This helps to create a feeling of community by working out with (or against) your friends, even if you can't physically be together. The app allows you to manage your own profile, friends list, track your best ghost runs, challenge other runners with similar average speeds, and track your location in real-time."
+          description="A mobile application that allows users to record their runs in the form of ghosts (playable recorded runs) which record the distance that a user travels at certain times and allows their runs to be replayed and challenged by other users that want to beat their times. "
           skills={["GCP", "JavaScript", "MongoDB", "React Native"]}
           links={[
             {
@@ -94,7 +94,7 @@ export default class Other extends Component {
           name="Fooder"
           event="eHacks"
           duration="Feb 2020"
-          description="An application that tracks personal grocery habits and nutritional information through user grocery purchases, highlights potential deficiencies, and suggests foods to fill the gaps to improve dietary health"
+          description="An application that tracks personal grocery habits and nutritional information through user grocery purchases, highlights potential deficiencies, and suggests foods to improve dietary health"
           skills={[
             "Bootstrap",
             "CSS",
@@ -156,7 +156,7 @@ export default class Other extends Component {
           name="Marks Blog"
           event=""
           duration="Dec 2019 - Jan 2020"
-          description="A full stack web application where users can create and manage their accounts, post blogs, and view the blogs of other users for any topic"
+          description="A web application where users can create and manage accounts, post blogs, and view the blogs of other users for any topic"
           skills={["Python", "Django", "HTML", "Bootstrap", "jQuery"]}
           links={[
             {
